b1103106152ddf6d1210a47551a963b4988c5f57
Adi Novitarini <adinovitarini@gmail.com>
2022-09-09 09:57:42 +0700

    Update combine_kf_vi.m

--- combine_kf_vi.m
+++ combine_kf_vi.m
@@ -1,9 +1,7 @@
 %% First Scenario : Kalman Filter and Value Iteration 
 function [P,u,x_hat,y,J,K,L] = combine_kf_vi(A,B,C,Q,R,N,L_kf,y)
 [P_vi,K_vi_old,~] = value_iteration(A,N,B,Q,R);
-mans = norm(K_vi_old);
-[~,idx] = max(mans);
-K_vi = K_vi_old(:,:,idx);
+K_vi = K_vi_old;
 x_hat_kf_vi = ones(4,1);
 for i = 1:N
     u_kf_vi(i) = -K_vi(i,:)*x_hat_kf_vi(:,i);
