1d591afceadf44daa7fac827f406b1b1f83fc986
Adi Novitarini <adinovitarini@gmail.com>
2022-10-25 10:02:58 +0700

    Update plant_one_tank.m

--- plant_one_tank.m
+++ plant_one_tank.m
@@ -1,53 +1,36 @@
-% clearvars -except data 
-tic
 clear all;clc
-z = tf('z');
-Gz = tf([0 0.08123],[1 -0.9895],0.01);
-[A,B,C,D] = tf2ss([0 0.08123],[1 -0.9895]);
+g = 9.8e-1;
+lp = 1.5;
+mp = 0.1;
+mk = 1;
+mt = 1.1;
+a = g/(lp*(4/3 -mp/(mp+mk)));
+A = [0 1 0 0;0 0 a 0;0 0 0 1;0 0 a 0];
+b = -1/(lp*(4/3 -mp/(mp+mk)));
+B = [0;1/mt;0;b];
+C = [0 1 0 1];
+D = 0;
 sys = ss(A,B,C,D,0.01);
 N = 100;
 u = 0;
 %%  Apply Disturbance 
 w = wgn(N,1,1);
 v = w;
-Rww = 1;
-Rvv = 1;
-% w = unifrnd(0,Rww,N,1);
-% v = unifrnd(0,Rvv,N,1);
 %%  Open Loop System
-x = .1*ones(size(A,1),1);
+x = .1*ones(4,1);
 for i = 1:N
     x(:,i+1) = A*x(:,i)+B*u+w(i);
     y(i) = C*x(:,i)+v(i);
     x(:,i) = x(:,i+1);
 end
+%% Weight Matrices 
+Rww = 0.01;
+Rvv = 1;
+Q = .01;
+R = .1;
 %%  LQG Conventional 
-R = Rvv;
-for i = 1:5
-    Rww(i) = 10^-i;
-    [~,LL(:,i),~] = kalman(sys,Rww(i),R);
-    [L(:,:,i),~] = KalmanConventional(sys,N,Rww(i),Rvv);
-    [K(i,:),~,cl(:,i)] = lqr(sys,Rww(i),R);
-end
-for i = 1:5
-    for j = 1:size(A,1)
-        L_norm(j,i) = norm(L(j,:,i));
-    end
-    L_normm(i) = norm(L_norm(:,i));
-end
-[val,idx] = min(L_normm);
-L_kf = L(:,:,idx);
-LL_kf = LL(:,idx);
-K_lqr = K(idx,:);
-%% Observer-based-controller 
-x_hat = ones(size(A,1),1,1);
-for i = 1:N
-    for j = 1:1
-        u_c(j,i) = -K_lqr(j,:)*x_hat(:,i,j);
-        y_hat(j,i) = C*x_hat(:,i,j);
-        x_hat(:,i+1,j) = (A-B*K_lqr(j,:))*x_hat(:,i,j)+L(:,j)*(y_hat(i)-y(i));
-    end
-end
+[~,L,~] = kalman(sys,Rww,Rvv);
+[K,~,cl] = lqr(sys,Q,R);
 %%  Observer via KalmanNet
 x_nw = ones(size(A,1),1);
 for i = 1:N
@@ -63,28 +46,29 @@ KG = KalmanNet(delta_x,target,A,C);
 delta_y = y+(-1*y_nw);
 x_hat_net = (KG*delta_y')+x_nw;
 y_hat_net = C*x_hat_net;
-%%  First scenario : KF-VI   
-Q = 1;
-R = .1;
-[P_kf_vi,u_kf_vi,x_hat_kf_vi,y_kf_vi,J_kf_vi,K_kf_vi,L_kf_vi] = combine_kf_vi(A,B,C,Q,R,N,L_kf,y);
-% for i = 1:size(x_hat_kf_vi,1)
-%     x_hat_kf_vi(i,:) = normalize(x_hat_kf_vi(i,:));
-% end
-%%  Second scenario : KalmanNet-LQR   
-% Implement KalmanNet-LQR 
+
+%% First scenario : LQG konvensional 
+x_lqg = x;
+x_hat_lqg = x;
+y_lqg = y;
+L_lqg = L;
+K_lqg = K;
+for i = 1:N
+    u_lqg(i) = -K_lqg*x_lqg(:,i);
+    x_hat_lqg(:,i+1) = A*x_hat_lqg(:,i)+B*u_lqg(i);
+    y_hat_lqg(i) = C*x_hat_lqg(:,i);
+    x_lqg(:,i+1) = (A-B*K_lqg)*x_lqg(:,i)+L_lqg*(y_hat_lqg(i)-y_lqg(i));
+    y_lqg(i) = C*x_lqg(:,i);
+end
+%%  Second scenario : KF-VI   
+[P_kf_vi,u_kf_vi,x_hat_kf_vi,y_kf_vi,J_kf_vi,K_kf_vi,L_kf_vi] = combine_kf_vi(A,B,C,Q,R,N,L,y);
+%%  Third scenario : KalmanNet-LQR   
 [P_kn_lqr,u_kn_lqr,x_hat_kn_lqr,y_kn_lqr,J_kn_lqr,K_kn_lqr] = combine_kn_lqr(A,B,C,Q,R,N,x_hat_net);
-% for i = 1:size(x_hat_kn_lqr,1)
-%     x_hat_kn_lqr(i,:) = normalize(x_hat_kn_lqr(i,:));
-% end
-%%  Third scenario : KalmanNet-VI
-%Implement Value Iteration 
+%%  Fourth scenario : KalmanNet-VI
 [P_kn_vi,u_kn_vi,x_hat_kn_vi,y_kn_vi,J_kn_vi,K_kn_vi] = combine_kn_vi(A,B,C,Q,R,N,x_hat_net);
-% for i = 1:size(x_hat_kn_vi,1)
-%     x_hat_kn_vi(i,:) = normalize(x_hat_kn_vi(i,:));
-% end
 %% Stability Analysis 
 for i = 1:N
-eig_kf_vi(:,i) = eig(A-B*K_kf_vi(i,:)-L_kf_vi(:,i)*C);
+eig_kf_vi(:,i) = eig(A-B*K_kf_vi(i,:)-L_kf_vi*C);
 eig_kn_lqr(:,i) = eig(A-B*K_kn_lqr-KG(1,i)*C);
 eig_kn_vi(:,i) = eig(A-B*K_kn_vi(i,:)-KG(1,i)*C);
 end
@@ -96,82 +80,11 @@ for j = 1:size(A,1)
     norm_eig_kn_lqr(j,1) = max(eig_kn_lqr(j,:));
     norm_eig_kn_vi(j,1) = max(eig_kn_vi(j,:));
 end
-time_elapsed = toc
-%% Display to command window 
+%% Display the output 
 clc
-fprintf('Norm Ac 1^{st} scenario : \n %f \n',norm_eig_kf_vi)
-fprintf('Norm Ac 2^{nd} scenario : \n %f \n',norm_eig_kn_lqr)
-fprintf('Norm Ac 3^{rd} scenario : \n %f \n',norm_eig_kn_vi)
-%%  Visualize the eigenvalues
-figure(1);clf
-plot(real(eig_kf_vi(1,:)),'-ob','markersize',2)
-hold on
-grid on
-% xlim([1 N])
-xlabel('Iteration Step')
-ylabel('$eig(A-BK-LC)$','Interpreter','latex')
-legend('$\hat{x}_1$','$\hat{x}_2$','$\hat{x}_3$','$\hat{x}_4$','Interpreter','latex')
-title('The Evolution of Eigenvalues on 1^{st} scenario : Combination KF and VI')
-figure(2);clf
-plot(real(eig_kn_lqr(1,:)),'-ob','markersize',2)
-hold on
-grid on
-% xlim([1 N])
-xlabel('Iteration Step')
-ylabel('$eig(A-BK-LC)$','Interpreter','latex')
-legend('$\hat{x}_1$','$\hat{x}_2$','$\hat{x}_3$','$\hat{x}_4$','Interpreter','latex')
-title('The Evolution of Eigenvalues on 2^{nd} scenario : Combination KN and LQR')
-figure(3);clf
-plot(real(eig_kn_vi(1,:)),'-ob','markersize',2)
-grid on
-% xlim([1 N])
-xlabel('Iteration Step')
-ylabel('$eig(A-BK-LC)$','Interpreter','latex')
-legend('$\hat{x}_1$','$\hat{x}_2$','$\hat{x}_3$','$\hat{x}_4$','Interpreter','latex')
-title('The Evolution of Eigenvalues on 3^{rd} scenario : Combination KN and VI')
-figure(4);clf
-plot(u_kf_vi','-ob','markersize',2);
-hold on
-plot(u_kn_lqr','-or','markersize',2);
-hold on
-plot(u_kn_vi','-ok','markersize',2);
-hold on
-grid on
-title('Control Signal')
-xlim([1 N])
-xlabel('Iteration step')
-ylabel('$u_k(x_k)$','Interpreter','latex')
-legend('1^{st} scenario : Combination KF-VI','2^{nd} scenario : Combination KN-LQR','3^{rd} scenario : Combination KN-VI')
-
-axes('Position',[0.35,0.35,0.35,0.35]);
-box on
-plot(u_kf_vi(:,1:4)','-ob','markersize',2);
-hold on
-plot(u_kn_lqr(:,1:4)','-or','markersize',2);
-hold on
-plot(u_kn_vi(:,1:4)','-ok','markersize',2);
-% set(gca,'xtick',[])
-set(gca,'ytick',[])
-figure(5);clf 
-title('The state trajectories')
-plot(x_hat_kf_vi(:,1:N),'-ob','markersize',2);
-hold on
-plot(x_hat_kn_lqr(:,1:N),'-or','markersize',2);
-hold on
-plot(x_hat_kn_vi(:,1:N),'-ok','markersize',2);
-hold on
-grid on
-xlabel('Iteration step')
-ylabel('$x_k$','Interpreter','latex')
-legend('1^{st} scenario : Combination KF-VI','2^{nd} scenario : Combination KN-LQR','3^{rd} scenario : Combination KN-VI')
-
-axes('Position',[0.35,0.35,0.35,0.35]);
-box on
-plot(x_hat_kf_vi(:,1:4),'-ob','markersize',2);
-hold on
-plot(x_hat_kn_lqr(:,1:4),'-or','markersize',2);
-hold on
-plot(x_hat_kn_vi(:,1:4),'-ok','markersize',2);
-hold on
-% set(gca,'xtick',[])
-set(gca,'ytick',[])
+fprintf('Kalman gain from KF : %f \n',norm(L))
+fprintf('Kalman gain from KN : %f \n',norm(KG))
+fprintf('Controller gain from LQR : %f \n',norm(K))
+fprintf('Controller gain from VI : %f \n',norm(K_kf_vi))
+fprintf('Controller gain from LQR : %f \n',norm(K_kn_lqr))
+fprintf('Controller gain from VI : %f \n',norm(K_kn_vi))
