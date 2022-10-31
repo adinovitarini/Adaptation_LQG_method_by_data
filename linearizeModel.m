bbd3f21ff6e93fe1ee651b7bda432cf371677886
Adi Novitarini <adinovitarini@gmail.com>
2022-10-31 09:59:35 +0700

    Update linearizeModel.m

--- linearizeModel.m
+++ linearizeModel.m
@@ -1,9 +1,12 @@
-%% Linearize
-function [A,B,C] = linearizeModel(net)
-nh = net.Layers(2).NumHiddenUnits
-Wa = net.Layers(2).RecurrentWeights(1:nh,:);
-Wb = net.Layers(2).InputWeights(1:nh,1);
-Wc = net.Layers(3).Weights;
+function [Model,x,y] = linearizeModel(trainedNetwork_3,consig)
+nh = trainedNetwork_3.Layers(2).NumHiddenUnits;
+Rw = trainedNetwork_3.Layers(2).RecurrentWeights;
+for i = 1:4
+w(:,:,i) = Rw(((i-1)*nh)+1:(i*nh),:);
+end
+Wa = w(:,:,3);
+Wb = trainedNetwork_3.Layers(2).InputWeights(1:nh,1);
+Wc = trainedNetwork_3.Layers(3).Weights;
 disp('The nonlinearity function : \n');
 disp('1. Sigmoid')
 disp('2. Tanh')
@@ -16,5 +19,21 @@ end
 end
 A = Phi*Wa;
 B = Phi*Wb;
-C = Wc*Phi;
+C = Wc;
+Model.A = A;
+Model.B = B;
+Model.C = C;
+x = zeros(nh,1);
+u = consig;
+if j==1
+    for i = 1:size(u,1)
+    x(:,i+1) = logsig(Wa*x(:,i)+Wb*u(i));
+    y(i) = Wc*x(:,i);
+    end
+else if j==2
+    for i = 1:size(u,1)
+    x(:,i+1) = tanh(Wa*x(:,i)+Wb*u(i));
+    y(i) = Wc*x(:,i);
+    end
+    end
 end
