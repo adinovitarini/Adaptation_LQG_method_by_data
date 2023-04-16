b59e9b9e486231bc68888018480477e0920f7362
Adi Novitarini <adinovitarini@gmail.com>
2023-04-16 10:15:08 +0700

    Update KalmanNet.m

--- KalmanNet.m
+++ KalmanNet.m
@@ -1,21 +1,15 @@
 function [KG,net] = KalmanNet(delta_x,target,u,A,B,C,N)
-%%  Normalize state trajectories
-% delta_x = [x_next_nw;y];
-% delta_x = x_next_nw;
 %%  Propagate 
 x_hat_kf = A*delta_x(1:end-1,:)+B*u;
 y_hat_kf = C*x_hat_kf;
-%%
 X = delta_x;
 Y = target(1:end-1,:);
 tic
 numFeatures = size(X,1);
 numResponses = size(Y,1);
-% numHiddenUnits = 1;
 numHiddenUnits = 3;
 layers = [ ...
     sequenceInputLayer(numFeatures)
-%     fullyConnectedLayer(numFeatures)
     lstmLayer(numHiddenUnits,'StateActivationFunction','tanh','GateActivationFunction','sigmoid')
     dropoutLayer(0.9,"Name","dropout")
     fullyConnectedLayer(numResponses)
@@ -33,13 +27,6 @@ options = trainingOptions('adam', ...
 net = trainNetwork(X,Y,layers,options);
 %% Predict 
 net = predictAndUpdateState(net,X);
-for i = 1:N
-    nets(i) = predictAndUpdateState(net,X(:,i));
-    KGG(:,i) = nets(i).Layers(2).HiddenState;
-end
 [net,KG] = predictAndUpdateState(net,target);
 time_elapsed = toc;
-% x_hat_next = (KG*Y')+x_hat_kf;
-% y_hat = C*x_hat_next;
 fprintf('MSE : %f\n',mse(KG,target(1,:)));
-% end
