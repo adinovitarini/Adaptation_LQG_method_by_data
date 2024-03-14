06bd419b57612dc585280917f0d03fc657e85b32
Adi Novitarini <adinovitarini@gmail.com>
2024-03-14 11:23:07 +0700

    Add files via upload

---
+++ GenerateSeq_VI.m
@@ -0,0 +1,36 @@
+function dataset = GenerateSeq_VI(sys,N,Rww,Rvv,Q,R)
+% This code is used for generate the sequence the estimated state that
+% obtain from KF and LQR 
+[L,~] = KalmanConventional(sys,N,Rww,Rvv);
+% [K,~,~] = lqr(sys,Q,R);
+[~,K,~] = value_iteration(sys.A,N,sys.B,Q,R);
+x = zeros(size(sys.A,1),N);
+x_hat = x;
+x_nw = x;
+y = zeros(size(sys.B,2),N);
+y_hat = y;
+y_nw = y;
+w = wgn(N,1,1);
+v = w;
+tic
+for i = 1:N
+    x(:,i+1) = (sys.A-sys.B*K(i))*x(:,i)+w(i);
+    y(i) = (sys.C*x(:,i))+v(i);
+    x(:,i) = x(:,i+1);
+    x_hat(:,i+1) = ((sys.A-sys.B*K(i))*x_hat(:,i))+L(i)*(y(i)-y_hat(i));
+    y_hat(i) = sys.C*x_hat(:,i);
+    x_hat(:,i) = x_hat(:,i+1);
+    x_nw(:,i+1) = (sys.A-sys.B*K(i))*x_nw(:,i);
+    y_nw(i) = (sys.C*x_nw(:,i));
+    x_nw(:,i) = x_nw(:,i+1);
+end
+time_elapsed = toc;
+u = -K*x_hat;
+dataset.x_hat = x_hat;
+dataset.x_nw = x_nw;
+dataset.x = x;
+dataset.y_hat = y_hat;
+dataset.y_nw = y_nw;
+dataset.y = y;
+dataset.u = u;
+dataset.time = time_elapsed;
